f01142edfdad17b1086baec033c1a4e61421f8be
jaelolpark <ac2cic@gmail.com>
2020-05-03 17:46:55 -0400

    todo list, clicking change color with line through

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,16 @@
-alert('hi');
+// Check off specific Todos by Cliking
+$('li').click(function() {
+  console.log($(this).css('color'));
+  if($(this).css('color')==='rgb(128, 128, 128)') {
+   $(this).css({
+     color: 'black',
+     textDecoration: 'none'
+    });
+  }
+  else {
+    $(this).css({
+      color: 'rgb(128, 128, 128)',
+      textDecoration: 'line-through'
+    });
+  } 
+});
