4ac32ed7362908fb9fbbf97be46fc279dfd68b0d
jaelolpark <ac2cic@gmail.com>
2020-05-03 18:14:31 -0400

    used toggleClass

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,16 +1,4 @@
 // Check off specific Todos by Cliking
 $('li').click(function() {
-  console.log($(this).css('color'));
-  if($(this).css('color')==='rgb(128, 128, 128)') {
-   $(this).css({
-     color: 'black',
-     textDecoration: 'none'
-    });
-  }
-  else {
-    $(this).css({
-      color: 'rgb(128, 128, 128)',
-      textDecoration: 'line-through'
-    });
-  } 
+  $(this).toggleClass('completed')
 });
