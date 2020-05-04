14517a14d0c40107d09ad6640b06a634151c023e
jaelolpark <ac2cic@gmail.com>
2020-05-04 00:16:20 -0400

    delete working

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,12 @@
 $('li').click(function() {
   $(this).toggleClass('completed')
 });
+
+// Click on X to delete Todo
+$('span').click(function(e) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  e.stopPropagation();
+
+})
