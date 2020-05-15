89dcdc38b56963d7e18f44bac03cdd1891f131e3
jaelolpark <ac2cic@gmail.com>
2020-05-14 21:46:28 -0400

    cross over working

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,5 @@
 // Check off specific Todos by Cliking
-$('li').click(function() {
+$('ul').on("click", 'li', function() {
   $(this).toggleClass('completed')
 });
 
@@ -12,12 +12,12 @@ $('span').click(function(e) {
 
 })
 
-  $("input[type='text'").keypress(function(e){
+  $("input[type='text']").keypress(function(e){
     if(e.which=== 13) {
       // grabbing new todo text from input
       var todoText = $(this).val();
       $(this).val("");
       // creat a new li and add to ul
-      $('ul').append('<li>' + todoText + '</li>')
+      $('ul').append('<li><span>X</span> ' + todoText + '</li>')
     }
   })
